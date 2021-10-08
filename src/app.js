952f54f060d24d19da7817c012e149b08370366a
npeyre <90184564+npeyre@users.noreply.github.com>
2021-10-08 15:12:08 +0200

    Added daily response to the function, integrated a loop for a real API data, introduced date timestamp function for week days, rounded forecast max/min temperature, integrated real data icon

--- src/app.js
+++ src/app.js
@@ -149,30 +149,41 @@ celsiusLink.addEventListener("click", showCelsiusTemperature);
 //Display weather forecast (note: API call is made in the function getForecast(coordinates), it is above function showTemperature(response))
 
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-
   let forecastHTML = '<div class="row">';
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
-            <div class="weather-forecast-day">${day}</div>
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+            <div class="weather-forecast-day">${formatDay(forecastDay.dt)}</div>
             <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png"
               alt=""
               width="30"
             />
             <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperature-max"> 25° </span>
-              <span class="weather-forecast-temperature-min"> 9° </span>
+              <span class="weather-forecast-temperature-max"> ${Math.round(
+                forecastDay.temp.max
+              )}° | </span>
+              <span class="weather-forecast-temperature-min"> ${Math.round(
+                forecastDay.temp.min
+              )}°</span>
             </div>
           </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
 }
