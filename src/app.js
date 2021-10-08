e61b2f01523a5c37d43b551372b36d38ed480478
npeyre <90184564+npeyre@users.noreply.github.com>
2021-10-09 00:50:18 +0200

    Resized weather forecast image

--- src/app.js
+++ src/app.js
@@ -142,7 +142,7 @@ function displayForecast(response) {
                 forecastDay.weather[0].icon
               }@2x.png"
               alt=""
-              width="30"
+              width="45"
             />
             <div class="weather-forecast-temperatures">
               <span class="weather-forecast-temperature-max"> ${Math.round(
